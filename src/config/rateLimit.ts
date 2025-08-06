edfe4fafb190c290b38b28870b7968bc5943b54b
manuelbent <manuelbent@pm.me>
2025-08-06 23:37:00 +0200

    add tests to /codes

--- src/config/rateLimit.ts
+++ src/config/rateLimit.ts
@@ -1,6 +1,9 @@
+import { Request } from 'express'
 import rateLimit from 'express-rate-limit'
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
+    keyGenerator: (req: Request) => req.ip ?? '::1',
+    message: 'Too many requests, please try again later.',
 })
--- src/routes/v1/__tests__/router.test.ts
+++ src/routes/v1/__tests__/router.test.ts
@@ -1,8 +1,23 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import app from '../../../app'
+import { limiter } from '../../../config/rateLimit'
 
 describe('/codes', () => {
+    beforeEach(() => {
+        limiter.resetKey('::ffff:127.0.0.1')
+    })
+
+    it('must block unauthorized requests', async () => {
+        const res = await request(app).post('/v1/codes').send({})
+        expect(res.status).toBe(401)
+    })
+
+    it('must block forbidden requests', async () => {
+        const res = await request(app).post('/v1/codes').set('Authorization', 'Bearer invalid').send({})
+        expect(res.status).toBe(403)
+    })
+
     it('must rate limit requests', async () => {
         for (let i = 0; i < 100; i++) {
             const res = await request(app).post('/v1/codes').send({})
