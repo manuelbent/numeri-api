ac65488d2b61acc2529a994b3f6bff668be65b48
manuelbent <manuelbent@pm.me>
2025-08-07 09:36:40 +0200

    add 200 test to /codes

--- src/routes/v1/__tests__/router.test.ts
+++ src/routes/v1/__tests__/router.test.ts
@@ -1,9 +1,24 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import 'dotenv/config'
+
+import { beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import request from 'supertest'
+import { OneTimeCode } from 'numeri-core'
 import app from '../../../app'
+import ioc from '../../../config/ioc'
 import { limiter } from '../../../config/rateLimit'
+import OneTimeCodeController from '../../../controllers/OneTimeCodeController'
+import OneTimeCodeServiceInterface from '../../../interfaces/OneTimeCodeServiceInterface'
 
 describe('/codes', () => {
+    const code = 'some-code'
+    const expiresAt = 'some-expires-at'
+
+    beforeAll(() => {
+        ioc.oneTimeCodeController = new OneTimeCodeController({
+            create: () => ({ code, expiresAt }) as unknown as Partial<OneTimeCode>,
+        } as OneTimeCodeServiceInterface)
+    })
+
     beforeEach(() => {
         limiter.resetKey('::ffff:127.0.0.1')
     })
@@ -26,5 +41,13 @@ describe('/codes', () => {
         const res = await request(app).post('/v1/codes').send({})
         expect(res.text).toBe('Too many requests, please try again later.')
         expect(res.status).toBe(429) // rate-limited
+    })
+
+    it('must return a one time code', async () => {
+        const res = await request(app).post('/v1/codes').set('Authorization', `Bearer ${process.env.ADMIN_BEARER}`).send({})
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveProperty('code', code)
+        expect(res.body).toHaveProperty('url', `${process.env.ROOT_URL}/v1/clients/register?code=${code}`)
+        expect(res.body).toHaveProperty('expiresAt', expiresAt)
     })
 })
