0ec16cf8e35d965254d8f0617223d998b382b70f
manuelbent <manuelbent@pm.me>
2025-09-03 14:46:51 +0200

    update core

--- src/interfaces/EventServiceInterface.ts
+++ src/interfaces/EventServiceInterface.ts
@@ -1,9 +1,9 @@
-import { RawEvent } from 'numeri-core'
+import { ProcessedEvent, RawEvent } from 'numeri-core'
 
 /**
  * @interface EventServiceInterface
  */
 export default interface EventServiceInterface {
     enqueue(data: object): Promise<RawEvent>
-    loadByClientId(id: number, query: Record<string, string|number>): Promise<RawEvent[]>
+    loadByClientId(id: number, query: Record<string, string|number>): Promise<ProcessedEvent[]>
 }
