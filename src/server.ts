4695ba6d6beb7440a7ba7a6b77cad2f4547a1b62
manuelbent <manuelbent@pm.me>
2025-11-24 13:05:51 +0100

    undo server changes

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,15 @@
 import 'dotenv/config'
 
-import os from 'os'
-import cluster from 'cluster'
 import { logger } from 'numeri-core'
-import { connect } from './config/redis'
 
 import app from './app'
+import { connect } from './config/redis'
 
 const port = Number(process.env.PORT) || 3000
 
-if (cluster.isPrimary) {
-    logger.info('Master process is running...')
-    logger.info(`Forking ${os.cpus().length} workers...`)
-    for (let i = 0; i < os.cpus().length; i++) {
-        cluster.fork()
-    }
+// connect to redis
+connect(process.pid).catch(logger.error)
 
-    cluster.on('exit', (worker, code, signal) => {
-        logger.warn(`Worker ${worker.process.pid} died (code: ${code}, signal: ${signal})`)
-        logger.info('Starting a new worker...')
-        cluster.fork()
-    })
-} else {
-    // connect to redis
-    connect(process.pid).catch(logger.error)
-    // start the server
-    app.listen(port, () => {
-        logger.info(`Worker ${process.pid} running on port ${port}...`)
-    })
-}
+app.listen(port, () => {
+    logger.info(`Server running on port ${port}...`)
+})
